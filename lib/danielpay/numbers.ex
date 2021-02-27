15d1048fa15c4318bd621fc504019351dabc2957
Daniel de Souza Neris <danielneris01@gmail.com>
2021-02-27 19:27:35 -0300

    feature(api): return ok or error in endpoint api/:filename

--- lib/danielpay/numbers.ex
+++ lib/danielpay/numbers.ex
@@ -15,5 +15,5 @@ defmodule Danielpay.Numbers do
     {:ok, %{result: result}}
   end
 
-  defp handle_file({:error, _reason}), do: {:error, "Invalid file"}
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file"}}
 end
--- lib/danielpay_web/router.ex
+++ lib/danielpay_web/router.ex
@@ -8,7 +8,7 @@ defmodule DanielpayWeb.Router do
   scope "/api", DanielpayWeb do
     pipe_through(:api)
 
-    get("/", WelcomeController, :index)
+    get("/:filename", WelcomeController, :index)
   end
 
   # Enables LiveDashboard only for development
