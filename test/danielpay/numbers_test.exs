215a6b94884379551d24a22cbef28b7c8e7d8821
Daniel de Souza Neris <danielneris01@gmail.com>
2021-02-27 19:56:40 -0300

    feature(test): create a test to route of sum numbers

---
+++ test/danielpay/numbers_test.exs
@@ -0,0 +1,23 @@
+defmodule Danielpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Danielpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "when there is a file with the given name, return the sum of numbers" do
+      response = Numbers.sum_from_file("numbers")
+
+      expected_response = {:ok, %{result: 37}}
+
+      assert response == expected_response
+    end
+
+    test "when there is no file with the given name, return an error" do
+      response = Numbers.sum_from_file("no-exists")
+
+      expected_response = {:error, %{message: "Invalid file"}}
+
+      assert response == expected_response
+    end
+  end
+end
